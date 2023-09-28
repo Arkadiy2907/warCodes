6c1de839bc41c0155f93077d290cb98986fad089
Arkadiy2907 <arkadiy2907@gmail.com>
2023-09-29 00:15:02 +0300

    added task

--- 64_order.js
+++ 64_order.js
@@ -35,3 +35,48 @@ function order(words) {
 console.log(order("is2 Thi1s T4est 3a"))//, "Thi1s is2 3a T4est");
 console.log(order("4of Fo1r pe6ople g3ood th5e the2"))//, "Fo1r the2 g3ood 4of th5e pe6ople");
 console.log(order(""))//,  "empty input should return empty string")
+
+// ============================================
+
+// https://www.codewars.com/kata/57d532d2164a67cded0001c7/train/javascript
+
+// Шестигранный кубик бросают несколько раз, и результаты отображаются в виде символьной гистограммы.
+
+//   Пример:
+
+// 6 |##### 5
+// 5 |
+// 4 |# 1
+// 3 |########## 10
+// 2 |### 3
+// 1 |####### 7
+
+// Задача
+// Вам будут переданы частоты значений кубика, и ваша задача — написать код, возвращающий строку,
+//  представляющую гистограмму, чтобы при печати она имела тот же формат, что и пример.
+
+//   Примечания
+// В строках нет конечных пробелов
+// Все строки(включая последнюю) заканчиваются новой строкой.\n
+// Рядом с каждой полосой отображается счетчик, за исключением случаев, когда счетчик равен 0.
+// Количество бросков может быть разным, но не более 100.
+
+var hist = histogram([7, 3, 10, 1, 0, 5]);
+
+function histogram(results) {
+  let res = [];
+
+  for (let i = 0; i < results.length; i++) {
+    if (results[i] !== 0) {
+      res.push(`${i + 1}|${'#'.repeat(results[i])} ${results[i]}\n`)
+    } else {
+      res.push(`${i + 1}|\n`)
+    }
+  }
+
+  return res.reverse().toString().split(',').join('');
+}
+
+var hist = histogram([7, 3, 10, 1, 0, 5]);
+
+console.log(hist);
