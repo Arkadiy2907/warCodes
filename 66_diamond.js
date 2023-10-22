a281acb678755c92969d6ca3e818e55a701ada0b
Arkadiy2907 <arkadiy2907@gmail.com>
2023-10-22 23:17:18 +0300

    added task

--- 66_diamond.js
+++ 66_diamond.js
@@ -117,3 +117,53 @@ const arr1 = [1, -2, -7, -4, 4, -2, 0, -3, 3];
 
 console.log(catchSignChange(arr));
 console.log(catchSignChange(arr1));
+
+// =====================================
+// https://www.codewars.com/kata/65127141a5de2b1dcb40927e/train/javascript
+
+// Given a list of directions to spin, "left" or "right", return an integer of how many full 360° rotations were made.Note that each word in the array counts as a 90° rotation in that direction.
+
+// Worked Example
+// ["right", "right", "right", "right", "left", "right"] ➞ 1
+// # You spun right 4 times(90 * 4 = 360)
+// # You spun left once(360 - 90 = 270)
+// # But you spun right once more to make a full rotation(270 + 90 = 360)
+// Examples
+// ["left", "right", "left", "right"] ➞ 0
+
+// ["right", "right", "right", "right", "right", "right", "right", "right"] ➞ 2
+
+// ["left", "left", "left", "left"] ➞ 1
+// Notes
+// Return a positive number.
+// All tests will only include the words "right" and "left".
+
+function spinAround(turns) {
+  let res = turns.reduce((acc, el) => {
+    if (el === 'right') return acc + 90
+    if (el === 'left') return acc - 90
+  }, 0)
+
+  return Math.trunc(Math.abs(res / 360))
+}
+
+// let turn = ["right", "right", "right", "right", "left", "right"]
+// let turn =['left', 'right', 'left', 'right']
+// let turn = ['right', 'right', 'right', 'right', 'right', 'right', 'right', 'right']
+let turn = ['left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left']
+let turn1 = ["right", "right", "right"]
+
+spinAround(turn)
+console.log(spinAround(turn1));
+
+function spinAround(turns) {
+
+  let result = 0;
+
+  turns.forEach(el => {
+    (el === 'right') ? result += 0.25 : result -= 0.25
+  });
+
+  return Math.floor(Math.abs(result));
+
+}
