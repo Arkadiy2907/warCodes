8db67c2c03d9798cd6ebe9491a3faa71d76b693a
Arkadiy2907 <arkadiy2907@gmail.com>
2023-12-29 20:06:09 +0300

    added task

--- 73_ast.js
+++ 73_ast.js
@@ -22,3 +22,32 @@ console.log(opposite(1));
 console.log(opposite(0));
 console.log(opposite(4.25));
 console.log(opposite(3.3333333));
+
+// =============================================
+//2) https://www.codewars.com/kata/basic-mathematical-operations
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2) {
+  const operations = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+  };
+
+  return operations[operation]
+    ? operations[operation](value1, value2)
+    : 'bed operation';
+}
+
+console.log(basicOp('+++', 4, 7));
