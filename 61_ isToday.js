449a308d96482033e28800142c7e766db1cad4ed
Arkadiy2907 <arkadiy2907@gmail.com>
2024-01-05 19:13:51 +0300

    added task

--- 61_ isToday.js
+++ 61_ isToday.js
@@ -6,8 +6,8 @@ function isToday(date) {
   let dateToday = new Date();
   let myDate = new Date(date);
   if (dateToday.getFullYear() !== myDate.getFullYear()) return false;
-  if (dateToday.getMonth() !== myDate.getMonth()) return false
-  return dateToday.getDate() === myDate.getDate()
+  if (dateToday.getMonth() !== myDate.getMonth()) return false;
+  return dateToday.getDate() === myDate.getDate();
 }
 
 function isToday(date) {
@@ -16,7 +16,11 @@ function isToday(date) {
 
 function isToday(date) {
   var d = new Date();
-  return (d.getDate() == date.getDate() && d.getFullYear() == date.getFullYear() && d.getMonth() == date.getMonth());
+  return (
+    d.getDate() == date.getDate() &&
+    d.getFullYear() == date.getFullYear() &&
+    d.getMonth() == date.getMonth()
+  );
 }
 
 // ==================================================
@@ -39,37 +43,61 @@ function isToday(date) {
 //   - 349000  =>  should return 'Tuesday 15:20'
 
 function dayAndTime(n) {
-
-  let dateSun = new Date(2023, 8, 10, 0, 0, 0, 0)
+  let dateSun = new Date(2023, 8, 10, 0, 0, 0, 0);
   // let dateSun = new Date(`2023-9-12`)
-  dateSun.setMinutes(dateSun.getMinutes() + n)
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  dateSun.setMinutes(dateSun.getMinutes() + n);
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
   ];
 
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
   ];
 
-  let monthNum = dateSun.getMonth()
+  let monthNum = dateSun.getMonth();
 
   // const [day, monthNum, year] = d.split('.');
   const month = months[parseInt(monthNum) - 1];
-  const day = days[dateSun.getDay()]
-  const hour = dateSun.getHours() < 10 ? `0${dateSun.getHours()}` : dateSun.getHours();
-  const min = dateSun.getMinutes() < 10 ? `0${dateSun.getMinutes()}` : dateSun.getMinutes();
-
+  const day = days[dateSun.getDay()];
+  const hour =
+    dateSun.getHours() < 10 ? `0${dateSun.getHours()}` : dateSun.getHours();
+  const min =
+    dateSun.getMinutes() < 10
+      ? `0${dateSun.getMinutes()}`
+      : dateSun.getMinutes();
 
   //enter your code here
-  return `${month} ${dateSun.getDate()}  ${day} ${hour}:${min}`
+  return `${month} ${dateSun.getDate()}  ${day} ${hour}:${min}`;
   // return dateSun.getDay()
 }
 
 function dayAndTime(n) {
   const date = new Date('March 17, 2019 00:00:00');
   date.setTime(date.getTime() + n * 60 * 1000);
-  return date.toLocaleDateString('en', { weekday: 'long', hour: 'numeric', minute: 'numeric', hour12: false });
+  return date.toLocaleDateString('en', {
+    weekday: 'long',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  });
 }
-
 
 console.log(dayAndTime(1));
 
@@ -85,9 +113,56 @@ const getNumDay = (d, m, y) => {
   let numDay = Math.floor(buf / (1000 * 60 * 60 * 24));
   let numWeek = Math.floor(buf / (1000 * 60 * 60 * 24 * 7));
 
-  return `номер дня в году:${numDay}, номер недели в году:${numWeek}`
+  return `номер дня в году:${numDay}, номер недели в году:${numWeek}`;
 };
 
 console.log(getNumDay(31, 7, 2023));
 
+// =============================================
+// https://www.codewars.com/kata/56eb16655250549e4b0013f4/train/javascript
 
+// Какой твой любимый день недели? Проверьте, является ли это самый частый день недели в году.
+// Вам дан год в виде целого числа (например, 2001). Вы должны вернуть наиболее часто встречающиеся дни недели в этом году. Результатом должен быть список дней, отсортированный по порядку дней недели (например ['Monday', 'Tuesday'], ['Saturday', 'Sunday'], , ['Monday', 'Sunday']). Неделя начинается с понедельника.
+// Входные данные: Год как целое число .
+// Вывод: список наиболее частых дней, отсортированный по порядку дней недели (с понедельника по воскресенье).
+// Предварительные условия:
+// Неделя начинается в понедельник.
+// Год между 1583 и 4000.
+// Календарь григорианский.
+// Примеры (ввод -> вывод):
+// * 2427 -> ['Friday']
+// * 2185 -> ['Saturday']
+// * 2860 -> ['Thursday', 'Friday']
+
+function mostFrequentDays(year) {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const obj = {};
+
+  for (let i = 0; i < 12; i++) {
+    for (let d = 1; d <= new Date(year, i + 1, 0).getDate(); d++) {
+      const dayOfWeek = new Date(year, i, d).getDay();
+      const dayName = days[dayOfWeek];
+
+      if (!obj[dayName]) {
+        obj[dayName] = 1;
+      } else {
+        obj[dayName]++;
+      }
+    }
+  }
+
+  const res = Object.keys(obj).filter((el) => obj[el] > 52);
+
+  return res[0] === 'Sunday' && res[1] === 'Monday' ? [res[1], res[0]] : res;
+}
+
+console.log(mostFrequentDays(1984)); //, ['Thursday', 'Friday'])
