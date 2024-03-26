eba6ea10ea907ffeba8f6e14464244c329c11384
Arkadiy2907 <arkadiy2907@gmail.com>
2024-03-26 17:29:39 +0300

    added task

--- 76_compareVersions.js
+++ 76_compareVersions.js
@@ -367,3 +367,30 @@ console.log(cube.volume); //  27
 const withoutLast = (arr) => arr.slice(0, arr.length - 1);
 
 console.log(withoutLast([90, 61, 33, 26, 54, 45, 52, 88, 19]));
+//https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+
+// Вы, наверное, знаете систему лайков по Facebook и другим страницам. Люди могут ставить лайки постам в блогах, изображениям и другим элементам. Мы хотим создать текст, который должен отображаться рядом с таким элементом.
+
+// Реализуйте функцию, которая принимает массив, содержащий имена людей, которым понравился предмет. Он должен возвращать отображаемый текст, как показано в примерах:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  if (names.length === 0) return 'no one likes this';
+  if (names.length === 1) return `${names[0]} likes this`;
+  if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  if (names.length === 3)
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  if (names.length >= 4)
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+}
+
+console.log(likes([])); //, 'no one likes this');
+console.log(likes(['Peter'])); // 'Peter likes this');
+console.log(likes(['Jacob', 'Alex'])); //, 'Jacob and Alex like this');
+console.log(likes(['Max', 'John', 'Mark'])); //   'Max, John and Mark like this' );
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max'])); //  'Alex, Jacob and 2 others like this'
